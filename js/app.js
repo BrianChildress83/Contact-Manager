4bf25b28bd4d42c99092fc58a2cf2aa20c2853ad
BrianChildress <brian@redchilidigital.com>
2013-09-30 19:48:37 -0600

    Upload JS dependencies, add style, img, custom JS

--- js/app.js
+++ js/app.js
@@ -11,4 +11,60 @@
         { name: "Contact 8", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "john@doe.com", type: "family" }
     ];
  
+
+
+
+// Create Backbone Model
+var Contact = Backbone.Model.extend({
+    defaults: {
+        photo: "img/defaultimg.png"
+    }
+});
+
+// Create Backbone Collection 
+var Directory = Backbone.Collection.extend({
+    model: Contact
+});
+
+// Individual contact view
+var ContactView = Backbone.View.extend({
+    tagName: "article",
+    className: "contact-container",
+    template: $("#contactTemplate").html(),
+ 
+    render: function () {
+        var tmpl = _.template(this.template);
+ 
+        this.$el.html(tmpl(this.model.toJSON()));
+        return this;
+    }
+});
+
+// Directory view
+var DirectoryView = Backbone.View.extend({
+    el: $("#contacts"),
+ 
+    initialize: function () {
+        this.collection = new Directory(contacts);
+        this.render();
+    },
+ 
+    render: function () {
+        var that = this;
+        _.each(this.collection.models, function (item) {
+            that.renderContact(item);
+        }, this);
+    },
+ 
+    renderContact: function (item) {
+        var contactView = new ContactView({
+            model: item
+        });
+        this.$el.append(contactView.render().el);
+    }
+});
+
+// Create Master view
+var directory = new DirectoryView();
+
 } (jQuery));
